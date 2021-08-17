886af813e6e5aa3118fb16f9cdc9d50e789c89f9
Maironㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <mairon31@programmer.net>
2021-08-17 00:57:39 -0600

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import * as bodyParser from 'body-parser'
 import ShortenerController from './controllers/shortener/controller'
 
 const app = new App({
-    port: process.env.PORT || 5000,
+    port: 3030,
     controllers: [
         new ShortenerController()
     ],
